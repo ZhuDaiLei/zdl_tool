46de7f8326ee823062b9fba978e8e292058f01b6
ZDL <zdl328465042@163.com>
2020-07-23 11:38:03 +0800

    导入index_widget

--- lib/ui/widget/index_widget.dart
+++ lib/ui/widget/index_widget.dart
@@ -1,4 +1,4 @@
-export 'package:zdl_tool/ui/widget/visibility_extend.dart';
+//export 'package:zdl_tool/ui/widget/visibility_extend.dart';
 export 'package:zdl_tool/ui/widget/line.dart';
 export 'package:zdl_tool/ui/widget/text_with_bg.dart';
 export 'package:zdl_tool/ui/widget/text_with_drawable.dart';
